a2ec503d2a5d54d6c21193ea804ec1ff713a7680
ayaznoori <ayaz.noori15@gmail.com>
2025-02-12 19:24:42 +0530

    normal fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-route
 import Login from "./components/Login";
 import "./styles/global.css";
 import Register from "./components/Register";
-import Home from "./pages/Home";
 import SubmissionsList from "./components/SubmisionList";
 import { ToastContainer } from "react-toastify";
 import ProtectedRoute from "./Routes/ProtectedRoutes";
@@ -11,6 +10,7 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Cookies from "js-cookie";
 import { logoutUser } from "./redux/actions/authActions";
+import AssignmentsList from "./pages/AssignmentList";
 
 function App() {
   const dispatch = useDispatch();
@@ -43,16 +43,16 @@ function App() {
   }, [dispatch, navigate]);
 
     return (
-        <>
+        <div>
             <Navbar/>
             <ToastContainer position="top-right" autoClose={2000} />
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+                <Route path="/" element={<ProtectedRoute><AssignmentsList /></ProtectedRoute>} />
                 <Route path="/assignment/:assessId" element={<ProtectedRoute><SubmissionsList /></ProtectedRoute>} />
                 <Route path="/register" element={<ProtectedRoute><Register /></ProtectedRoute>} />
             </Routes>
-        </>
+        </div>
     );
 }
 
